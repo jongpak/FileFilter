22683a20c606b31270b4914f0ea6b3603cf113e3
jongpak <jplabs@canxan.com>
2016-11-19 14:37:56 +0900

    Add a PickExtention filter

---
+++ src/com/jongpak/filefilter/filters/PickExtention.java
@@ -0,0 +1,27 @@
+package com.jongpak.filefilter.filters;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+
+import com.jongpak.filefilter.Filter;
+
+public class PickExtention extends Filter {
+    protected HashSet<String> extentions = new HashSet<>();
+
+    public PickExtention() {
+        addOptionHandler("extention", extention -> extentions.add(((String) extention).toLowerCase()));
+    }
+
+    @Override
+    public List<File> filter(File file) {
+        return extentions.contains(getExtention(file)) || file.isDirectory() ? pass(file) : null;
+    }
+
+    protected String getExtention(File file) {
+        String fileName = file.getName();
+        int pos = fileName.lastIndexOf(".");
+
+        return pos != -1 ? fileName.substring(pos + 1) : "";
+    }
+}
---
+++ tests/com/jongpak/filefilter/test/filter/PickExtentionTest.java
@@ -0,0 +1,23 @@
+package com.jongpak.filefilter.test.filter;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.jongpak.filefilter.Filter;
+import com.jongpak.filefilter.filters.PickExtention;
+
+public class PickExtentionTest {
+    @Test
+    public void test() throws Exception {
+        Filter filter = new PickExtention();
+        filter.option("extention", "txt");
+
+        assertEquals(1, filter.filter(new File("test.txt")).size());
+        assertEquals(null, filter.filter(new File("test.doc")));
+        assertEquals(null, filter.filter(new File("test")));
+        assertEquals(null, filter.filter(new File("test/")));
+    }
+}
