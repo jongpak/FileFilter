51d43880959049913b2d792d3c9ad07fa7624807
jongpak <jplabs@canxan.com>
2016-11-19 14:37:56 +0900

    Change specific class to general class type in IntroFilterTest

--- tests/com/jongpak/filefilter/test/filter/IntroFilterTest.java
+++ tests/com/jongpak/filefilter/test/filter/IntroFilterTest.java
@@ -6,15 +6,15 @@ import java.io.File;
 
 import org.junit.Test;
 
+import com.jongpak.filefilter.Filter;
 import com.jongpak.filefilter.filters.IntroFilter;
 
 public class IntroFilterTest {
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         File file = new File("test.txt");
-
-        IntroFilter filter = new IntroFilter();
+        Filter filter = new IntroFilter();
 
         assertEquals(1, filter.filter(file).size());
         assertEquals(file, filter.filter(file).get(0));
